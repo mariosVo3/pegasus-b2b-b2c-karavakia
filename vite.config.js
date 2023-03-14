dfed2ecfdfbb8c07bed40a2fe20410dd146c1114
PenyPip <penynikolaidou@gmail.com>
2023-03-14 16:15:24 +0200

    new

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { Https } from '@mui/icons-material'
+import https from 'https'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()], 
-
   server: {
     host: true,
-    Https:true,
-    port:5174
+    port:5174,
+    
+    proxy: {
+      
+      '/api': {
+        target: 'https://infounit.ten06.eu/pegasus/api',
+        changeOrigin: true,
+        open: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure:false,
+        
+
+      },
+
+    
+      
+      }
+     
+    
   }
 })
